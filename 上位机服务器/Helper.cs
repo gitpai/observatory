b413881caa0fef163221c0a09f246b284906b278
gitpai <iwuyujie@126.com>
2017-05-25 17:05:49 +0800

    version

---
+++ 上位机服务器/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 上位机服务器
+{
+    class Helper
+    {
+        /// <summary>
+        /// 获取本机IP地址
+        /// </summary>
+        /// <returns></returns>
+        public static String getLocalAddress()
+        {
+            return "192.168.1.101";
+        }
+    }
+}
